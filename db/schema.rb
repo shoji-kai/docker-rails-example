fa909b2ddd6924e5300006b251c91166063511bb
Shoji Kai <shojikai.dev@gmail.com>
2026-02-20 06:51:20 +0900

    add devise authentication for admin and customer models
    デバイスのインストール
    
    ```
    bundle add devise
    bin/rails g devise:install
    ```
    
    モデルの作成
    
    ```
    bin/rails g devise Admin
    bin/rails g devise Customer
    bin/rails db:migrate
    ```
    
    コントローラの作成
    
    ```
    bin/rails g devise:controllers admin
    bin/rails g devise:controllers customer
    ```
    
    ビューの作成
    
    ```
    bin/rails g devise:views admin
    bin/rails g devise:views customer
    ```

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
-
+ActiveRecord::Schema[8.1].define(version: 2026_02_19_213521) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
+  create_table "admins", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "name", null: false
+    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
+    t.integer "status", default: 0, null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_customers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
 end
